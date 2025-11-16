47937633ca43e6ce1ba7483c6f4b6f89b91e20d4
Emily Van Tries <erv1029@gmail.com>
2025-11-16 11:23:41 -0500

    added more css

--- src/utils/simulateFight.js
+++ src/utils/simulateFight.js
@@ -1,9 +1,19 @@
-// utils/simulateFight.js
 export const simulateFight = (heroA, heroB) => {
   const winner = Math.random() < 0.5 ? heroA : heroB;
   const loser = winner === heroA ? heroB : heroA;
 
-  const moves = ['thunder punch', 'laser blast', 'telekinetic slam', 'shadow strike'];
+ const moves = [
+  'Power Punch',
+  'Energy Blast',
+  'Speed Dash',
+  'Aerial Slam',
+  'Shield Block',
+  'Teleport Strike',
+  'Mind Shock',
+  'Elemental Surge',
+  'Weapon Whirl',
+  'Combo Barrage'
+];
   const move = moves[Math.floor(Math.random() * moves.length)];
 
   const description = `${winner.name} uses ${move} to defeat ${loser.name} in a fierce battle!`;
